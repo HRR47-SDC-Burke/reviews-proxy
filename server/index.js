71d8db6b25b0ae255ce1f9a1fdd6dbdd0a05c0d0
Steven Walker <swalk001@gmail.com>
2020-09-03 22:54:35 -0400

    set up proxy for stress testing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/../client')));
@@ -12,29 +14,29 @@ app.use('/:id', express.static(path.join(__dirname, '/../client')));
 
 app.use(bodyParser.json());
 
-app.get('/api/images/:id', (req, res) => {
-  const id = req.url.slice(12);
-  axios.get(`http://localhost:3001/api/images/${id}`)
-    .then((data) => res.send(data.data))
-    .catch((error) => console.log(error));
-});
+// app.get('/api/images/:id', (req, res) => {
+//   const id = req.url.slice(12);
+//   axios.get(`http://localhost:3001/api/images/${id}`)
+//     .then((data) => res.send(data.data))
+//     .catch((error) => console.log(error));
+// });
 
-app.get('/api/booking/:id', (req, res) => {
-  const id = req.url.slice(13);
-  axios.get(`http://localhost:3002/api/booking/${id}`)
-    .then((data) => res.send(data.data))
-    .catch((error) => console.log(error));
-});
+// app.get('/api/booking/:id', (req, res) => {
+//   const id = req.url.slice(13);
+//   axios.get(`http://localhost:3002/api/booking/${id}`)
+//     .then((data) => res.send(data.data))
+//     .catch((error) => console.log(error));
+// });
 
 app.get('/api/overall_reviews/:id', (req, res) => {
-  const id = req.url.slice(21);
+  const id = req.params.id;
   axios.get(`http://localhost:3003/api/overall_reviews/${id}`)
     .then((data) => res.send(data.data))
     .catch((error) => console.log(error));
 });
 
 app.get('/api/individual_reviews/:id', (req, res) => {
-  const id = req.url.slice(24);
+  const id = req.params.id;
   axios.get(`http://localhost:3003/api/individual_reviews/${id}`)
     .then((data) => res.send(data.data))
     .catch((error) => console.log(error));
